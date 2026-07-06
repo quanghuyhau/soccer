3f1a488fda4b4d20b627bf9a67658af067182698
huytq <huyquangtr1105@gmail.com>
2026-07-06 15:43:54 +0700

    aaa

---
+++ lib/src/app/use_cases/venues/delete_pitch_price_use_case.dart
@@ -0,0 +1,11 @@
+import '../../repositories/pitch_repository.dart';
+
+class DeletePitchPriceUseCase {
+  const DeletePitchPriceUseCase(this._repository);
+
+  final PitchRepository _repository;
+
+  Future<void> call(String priceId) {
+    return _repository.deletePitchPrice(priceId);
+  }
+}
---
+++ lib/src/app/use_cases/venues/update_pitch_price_use_case.dart
@@ -0,0 +1,22 @@
+import '../../models/app_models.dart';
+import '../../repositories/pitch_repository.dart';
+
+class UpdatePitchPriceParams {
+  const UpdatePitchPriceParams({required this.priceId, required this.request});
+
+  final String priceId;
+  final CreatePitchPriceRequest request;
+}
+
+class UpdatePitchPriceUseCase {
+  const UpdatePitchPriceUseCase(this._repository);
+
+  final PitchRepository _repository;
+
+  Future<PitchPrice> call(UpdatePitchPriceParams params) {
+    return _repository.updatePitchPrice(
+      priceId: params.priceId,
+      request: params.request,
+    );
+  }
+}
